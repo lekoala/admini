bd076a309db5eb5fb87496ad98f4d334d4b5158a
Thomas <thomas@lekoala.be>
2023-04-21 13:10:33 +0200

    improve form demo

--- static/php/submit-form.php
+++ static/php/submit-form.php
@@ -2,8 +2,15 @@
 
 sleep(random_int(0, 2)); // simulate load time
 
-header('X-Status: "posted at '.date('Y-m-d H:i:s').'"');
+$fname = $_POST['fname'] ?? null;
+$lname = $_POST['lname'] ?? null;
+
+header('X-Status: posted at ' . date('Y-m-d H:i:s') . ' for ' . $fname . ' ' . $lname);
 
 $content = file_get_contents('../forms-simple.html');
+
+// This is where you would replace the value
+$content = str_replace('name="fname" ', 'name="fname" value="' . $fname . ' (from server)' . '"', $content);
+$content = str_replace('name="lname" ', 'name="lname" value="' . $lname . ' (from server)' . '"', $content);
 
 echo $content;
